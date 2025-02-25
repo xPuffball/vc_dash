e312bc787fcc81d23037e63dfb3255ff2fd6c3cc
Sirtaj Sing <s398sing@uwaterloo.ca>
2025-02-24 22:08:42 -0500

    Added q3 dataset

---
+++ src/data/questionThreeData.js
@@ -0,0 +1,221 @@
+
+export const avgInvestmentByRound = [
+    {
+        "year": 2019,
+        "Grant": 35000,
+        "Pre-Seed": 291142.9512195122,
+        "Seed": 1633169.8942731277,
+        "Series A": 16712926.234042553,
+        "Series B": 28655329.02222222,
+        "Series C": 31839422.727272727,
+        "Series D": 100444031.81818181
+    },
+    {
+        "year": 2020,
+        "Grant": 825000,
+        "Pre-Seed": 251051.3787878788,
+        "Seed": 1798664.391509434,
+        "Series A": 11112212.896551725,
+        "Series B": 25840755.84,
+        "Series C": 41186071.428571425,
+        "Series D": 26057728
+    },
+    {
+        "year": 2021,
+        "Grant": 4133092.4583333335,
+        "Pre-Seed": 445817.5084745763,
+        "Seed": 3048912,
+        "Series A": 17446857.12962963,
+        "Series B": 59180756.8627451,
+        "Series C": 129768634.72222222,
+        "Series D": 220398514.2857143
+    },
+    {
+        "year": 2022,
+        "Grant": 2475000,
+        "Pre-Seed": 1766867.566037736,
+        "Seed": 3976754.64375,
+        "Series A": 19693065.45882353,
+        "Series B": 48162592.741935484,
+        "Series C": 140619823.33333334,
+        "Series D": 232833438
+    },
+    {
+        "year": 2023,
+        "Grant": 1000000,
+        "Pre-Seed": 1452006.4444444445,
+        "Seed": 5836519.323076923,
+        "Series A": 16300637.551020408,
+        "Series B": 43980872.69565217,
+        "Series C": 96084425,
+        "Series D": 104705850
+    },
+    {
+        "year": 2024,
+        "Grant": 27100000,
+        "Pre-Seed": 1822142.857142857,
+        "Seed": 4988761.946666666,
+        "Series A": 18433320.739130434,
+        "Series B": 43886403.14285714,
+        "Series C": 57764975,
+        "Series D": 435500000
+    },
+    {
+        "year": 2025,
+        "Grant": 0,
+        "Pre-Seed": 887500,
+        "Seed": 3897144.1428571427,
+        "Series A": 15500000,
+        "Series B": 66500000,
+        "Series C": 28633333.333333332,
+        "Series D": 0
+    }
+]
+
+export const totalInvestmentByRound = [
+    {
+        "year": 2019,
+        "Grant": 105000,
+        "Pre-Seed": 35810583,
+        "Seed": 370729566,
+        "Series A": 785507533,
+        "Series B": 1289489806,
+        "Series C": 350233650,
+        "Series D": 1104884350
+    },
+    {
+        "year": 2020,
+        "Grant": 4950000,
+        "Pre-Seed": 16569391,
+        "Seed": 381316851,
+        "Series A": 966762522,
+        "Series B": 646018896,
+        "Series C": 576605000,
+        "Series D": 130288640
+    },
+    {
+        "year": 2021,
+        "Grant": 99194219,
+        "Pre-Seed": 52606466,
+        "Seed": 771374736,
+        "Series A": 1884260570,
+        "Series B": 3018218600,
+        "Series C": 2335835425,
+        "Series D": 1542789600
+    },
+    {
+        "year": 2022,
+        "Grant": 9900000,
+        "Pre-Seed": 93643981,
+        "Seed": 636280743,
+        "Series A": 1673910564,
+        "Series B": 1493040375,
+        "Series C": 2109297350,
+        "Series D": 1164167190
+    },
+    {
+        "year": 2023,
+        "Grant": 1000000,
+        "Pre-Seed": 39204174,
+        "Seed": 379373756,
+        "Series A": 798731240,
+        "Series B": 1011560072,
+        "Series C": 768675400,
+        "Series D": 418823400
+    },
+    {
+        "year": 2024,
+        "Grant": 81300000,
+        "Pre-Seed": 51020000,
+        "Seed": 374157146,
+        "Series A": 847932754,
+        "Series B": 921614466,
+        "Series C": 462119800,
+        "Series D": 1742000000
+    },
+    {
+        "year": 2025,
+        "Grant": 0,
+        "Pre-Seed": 3550000,
+        "Seed": 27280009,
+        "Series A": 108500000,
+        "Series B": 266000000,
+        "Series C": 85900000,
+        "Series D": 0
+    }
+]
+
+export const countInvestmentByRound = [
+    {
+        "year": 2019,
+        "Grant": 3,
+        "Pre-Seed": 123,
+        "Seed": 227,
+        "Series A": 47,
+        "Series B": 45,
+        "Series C": 11,
+        "Series D": 11
+    },
+    {
+        "year": 2020,
+        "Grant": 6,
+        "Pre-Seed": 66,
+        "Seed": 212,
+        "Series A": 87,
+        "Series B": 25,
+        "Series C": 14,
+        "Series D": 5
+    },
+    {
+        "year": 2021,
+        "Grant": 24,
+        "Pre-Seed": 118,
+        "Seed": 253,
+        "Series A": 108,
+        "Series B": 51,
+        "Series C": 18,
+        "Series D": 7
+    },
+    {
+        "year": 2022,
+        "Grant": 4,
+        "Pre-Seed": 53,
+        "Seed": 160,
+        "Series A": 85,
+        "Series B": 31,
+        "Series C": 15,
+        "Series D": 5
+    },
+    {
+        "year": 2023,
+        "Grant": 1,
+        "Pre-Seed": 27,
+        "Seed": 65,
+        "Series A": 49,
+        "Series B": 23,
+        "Series C": 8,
+        "Series D": 4
+    },
+    {
+        "year": 2024,
+        "Grant": 3,
+        "Pre-Seed": 28,
+        "Seed": 75,
+        "Series A": 46,
+        "Series B": 21,
+        "Series C": 8,
+        "Series D": 4
+    },
+    {
+        "year": 2025,
+        "Grant": 0,
+        "Pre-Seed": 4,
+        "Seed": 7,
+        "Series A": 7,
+        "Series B": 4,
+        "Series C": 3,
+        "Series D": 0
+    }
+]
+
+export const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
